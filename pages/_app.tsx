e430a55ff11768e5193809e5be5c22021d551dea
zman811 <sunburnzman@gmail.com>
2022-12-11 11:06:29 -0700

    Added in stlyes setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,41 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from 'next/app';
+import Head from 'next/head';
+import { MantineProvider } from '@mantine/core';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Page title</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          colorScheme: "dark",
+          colors: {
+            brand: [
+              "#F0BBDD",
+              "#ED9BCF",
+              "#EC7CC3",
+              "#ED5DB8",
+              "#F13EAF",
+              "#F71FA7",
+              "#FF00A1",
+              "#E00890",
+              "#C50E82",
+              "#AD1374",
+            ],
+          },
+          primaryColor: "brand",
+        }}
+      >
+        <Component {...pageProps} />
+      </MantineProvider>
+    </>
+  );
 }
