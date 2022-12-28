d26ac10e548d426e0a1ca3cc154e5d07c474755e
zman811 <sunburnzman@gmail.com>
2022-12-28 11:44:04 -0700

    Changes to new page

--- pages/tech.tsx
+++ pages/tech.tsx
@@ -1,9 +1,20 @@
+import NavBar from "../components/NavBar";
+import Head from "next/head";
+import { Center, Title } from "@mantine/core";
+
 export default function Tech() {
   return (
     <>
-    <main>
-      <div>Hello</div>
-    </main>
+      <Head>
+        <title>Tech-Stack</title>
+        <meta name="description" content="Personal website techStack" />
+      </Head>
+      <NavBar />
+      <main>
+        <Center>
+          <Title order={2}>Hello</Title>
+        </Center>
+      </main>
     </>
-  )
+  );
 }
