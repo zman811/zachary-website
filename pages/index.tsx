3da714ebc187122ec098436d41a435199d36d474
zman811 <sunburnzman@gmail.com>
2023-01-05 12:44:37 -0700

    Disabled button

--- pages/index.tsx
+++ pages/index.tsx
@@ -96,7 +96,7 @@ export default function Home() {
                 <Space h="lg" />
                 <Center>
                   <Link href="/tech">
-                  <Button style={{ margin: "0.5em" }}>
+                  <Button disabled style={{ margin: "0.5em" }}>
                     Why the tech stack?
                   </Button>
                   </Link>
